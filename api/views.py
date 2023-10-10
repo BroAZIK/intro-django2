090aff40ce48131d81afc4c9c2c681d3ba25527f
djumanov <diyorbekjumanov1120@gmail.com>
2023-10-10 10:40:22 +0500

    app

---
+++ api/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse, HttpRequest
+
+
+def sum(request: HttpRequest) -> HttpResponse:
+
+    if request.method == 'GET':
+        a = request.GET.get('a')
+        b = request.GET.get('b')
+
+        result = int(a) + int(b)
+
+    return HttpResponse(f"{a} + {b} = {result}")
+
+
+def hi(request: HttpRequest, name: str) -> HttpResponse:
+
+    return HttpResponse(f"Hi {name}")
+    
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,20 +1,5 @@
 from django.urls import path
-from django.http import HttpResponse, HttpRequest
-
-
-def sum(request: HttpRequest) -> HttpResponse:
-
-    if request.method == 'GET':
-        a = request.GET.get('a')
-        b = request.GET.get('b')
-
-        result = int(a) + int(b)
-
-    return HttpResponse(f"{a} + {b} = {result}")
-
-def hi(request: HttpRequest, name: str) -> HttpResponse:
-
-    return HttpResponse(f"Hi {name}")
+from api.views import sum, hi
 
 
 urlpatterns = [
